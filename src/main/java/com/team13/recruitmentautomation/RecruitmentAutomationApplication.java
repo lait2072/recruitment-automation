dbb264c0bc55091eef28d728b9776e8cdd9d1c27
lait2072 <91603917+lait2072@users.noreply.github.com>
2023-11-01 16:52:01 +0300

    Initial commit

---
+++ src/main/java/com/team13/recruitmentautomation/RecruitmentAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.team13.recruitmentautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentAutomationApplication.class, args);
+	}
+
+}
