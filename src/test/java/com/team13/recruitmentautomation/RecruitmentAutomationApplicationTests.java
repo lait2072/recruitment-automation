9a7fe6385a152b0d67a416dcbb8b713e55c0401c
Albina <albina.melsitova@mail.ru>
2023-11-23 11:28:38 +0300

    Uploaded the project

---
+++ src/test/java/com/team13/recruitmentautomation/RecruitmentAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team13.recruitmentautomation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
