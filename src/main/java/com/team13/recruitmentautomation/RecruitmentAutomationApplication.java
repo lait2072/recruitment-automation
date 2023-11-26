604e3509ef00042fa0a8c6b4a8bb79ed276597ce
lait2072 <91603917+lait2072@users.noreply.github.com>
2023-11-26 17:11:20 +0300

    Добавлен метод sendWhatsUpMessage в класс WhatsAppSend для отправки текстовых сообщений через WhatsApp. Теперь возможно использовать этот метод для отправки сообщений по списку номеров, используя forEach в главном классе.

--- src/main/java/com/team13/recruitmentautomation/RecruitmentAutomationApplication.java
+++ src/main/java/com/team13/recruitmentautomation/RecruitmentAutomationApplication.java
@@ -3,11 +3,23 @@ package com.team13.recruitmentautomation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class RecruitmentAutomationApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(RecruitmentAutomationApplication.class, args);
+		ArrayList<String> listNumber = new ArrayList();
+		listNumber.add("+79253367293");
+		listNumber.add("+79653105170");
+		listNumber.forEach(number -> {
+			try {
+				WhatsAppSend.sendWhatsUpMessage(number, "какой-то текст");
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
 	}
 
 }
